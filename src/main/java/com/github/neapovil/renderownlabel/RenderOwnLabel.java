615b26d9a749e46e66e0881843fdb2e5aa89f55e
William <nearata@protonmail.com>
2024-07-05 22:52:22 +0200

    init mod

---
+++ src/main/java/com/github/neapovil/renderownlabel/RenderOwnLabel.java
@@ -0,0 +1,16 @@
+package com.github.neapovil.renderownlabel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class RenderOwnLabel implements ClientModInitializer
+{
+    public static final Logger LOGGER = LoggerFactory.getLogger("RenderOwnLabel");
+
+    @Override
+    public void onInitializeClient()
+    {
+    }
+}
